6fa098bd57c51ba7b54a9bb13c54220466c127e7
Hazri <hazrijordan@gmail.com>
2019-07-31 21:29:40 +0800

    My solution to coin flip challenge. Image may still not be optimised but moving on to the next

--- src/Coin.js
+++ src/Coin.js
@@ -13,26 +13,56 @@ class Coin extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0
+      array: [],
+      counter: 0,
+      heads: 0,
+      tails: 0
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
+  // flipCoin() {
+  //   const result = Math.floor(Math.random() * this.props.images.length) + 1;
+  // }
+
   handleClick() {
+    const result = Math.floor(Math.random() * this.props.images.length) + 1;
+
     this.setState({
+      array: result,
       counter: this.state.counter + 1
     });
+    if (this.state.array === 1) {
+      this.setState({
+        heads: this.state.heads + 1
+      });
+      return (
+        <img src={this.props.images[0]} alt="heads" className="Coin-face" />
+      );
+    } else {
+      this.setState({
+        tails: this.state.tails + 1
+      });
+      return (
+        <img src={this.props.images[1]} alt="tails" className="Coin-face" />
+      );
+    }
   }
   render() {
     return (
       <section className="Coin">
         <h1>{this.props.title}</h1>
         <div>
-          <img src={this.props.images[0]} alt="head" className="Coin-face" />
-          <img src={this.props.images[1]} alt="head" className="Coin-face" />
+          {this.state.array === 1 ? (
+            <img src={this.props.images[0]} alt="heads" className="Coin-face" />
+          ) : (
+            <img src={this.props.images[1]} alt="tails" className="Coin-face" />
+          )}
         </div>
         <button onClick={this.handleClick}>Flip the coin!</button>
         <p>Amount of flip: {this.state.counter}</p>
+        <p>Amount of heads flipped: {this.state.heads}</p>
+        <p>Amount of tails flipped: {this.state.tails}</p>
       </section>
     );
   }
